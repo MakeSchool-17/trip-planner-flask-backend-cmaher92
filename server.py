ba0b6222f3a2b4994a880f9a17f2cd6e9f2122dc
cmaher92 <connor.maher@students.makeschool.com>
2015-10-19 12:45:01 -0700

    Finished http actions, now need to add auth.

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from utils.mongo_json_encoder import JSONEncoder
+import bcrypt
 
 # Basic Setup
 app = Flask(__name__)                    # create flask instance and assign var
@@ -48,6 +49,31 @@ class Trip(Resource):
         return updated
 
     # delete trip
+    def delete(self, trip_id):
+        trip_collection = app.db.trips
+        result = trip_collection.delete_one({'_id': ObjectId(trip_id)})
+        deleted_trip = trip_collection.find_one_or_404(
+            {'_id': ObjectId(trip_id)})
+        return deleted_trip
+
+class User(Resource):
+
+    def post(self):
+        user = request.json  # requests json doc
+        user_collection = app.db.users  # collection of users to store
+        encoded_password = user['password'].encode('utf-8')  # encodes pass
+        hashed_password = bcrypt.hashpw(encoded_password,  # hashes password
+                                        bcrypt.gensalt(app.bcrypt_rounds))
+        user['password'] = hashed_password  # updates
+        user_collection.insert_one(user)  # inserts
+        return
+
+    def get(self):
+        resp = jsonify(message=[])
+        resp.status_code = 200
+        return resp
+
+
 
 
 
@@ -55,7 +81,8 @@ class Trip(Resource):
 # Route defines a URL that can be called by a client application
 # Parameter 1: resource which we want ot map to a specific URL
 # Parameter 2/3: one is used to create an instance the other is used to retrieve a specific instance
-api.add_resource(MyObject, '/myobject/','/myobject/<string:myobject_id>')
+api.add_resource(Trip, '/trip/', '/trip/<string:trip_id>')
+api.add_resource(User, '/user/', '/user/<string:user_id>')
 
 # provide a custom JSON serializer for flaks_restful
 # JSON encoder takes python objects and turns them into JSON text representation
